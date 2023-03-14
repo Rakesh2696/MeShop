ad58043555a5111ca9915ccf673da7854916fac0
rakesh2696 <rakesh.sanaboina@gmail.com>
2023-03-14 18:28:42 +0530

    Add project files.

---
+++ MeShop/Controllers/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeShop.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
