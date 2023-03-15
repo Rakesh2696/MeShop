8a85874ff8c53f48d3007cda940f64a99fb71261
Ganesh <ganeshkumarankani@gmail.com>
2023-03-15 19:53:50 +0530

    Adding Footer and Header in User_Master

---
+++ MeShop/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeShop.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
