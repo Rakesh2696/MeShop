c8bae30faaef118673b39769e9dad299308d7ac3
mouli <Hp@DESKTOP-K3NBKKB>
2023-03-17 20:06:31 +0530

    adding savedata and login method

--- MeShop/Controllers/MainController.cs
+++ MeShop/Controllers/MainController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using MeShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
+
+
 
 namespace MeShop.Controllers
 {
+    
     public class MainController : Controller
     {
+        MeShopEntities meShop=new MeShopEntities();
         // GET: Main
         public ActionResult LogIn()
         {
@@ -16,6 +22,34 @@ namespace MeShop.Controllers
         public ActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public JsonResult Savedata(SigIn registerdata)
+        {
+            bool result = false;
+            try 
+            {
+                meShop.SigIns.Add(registerdata);
+                meShop.SaveChanges();
+                result = true;
+            }
+            catch(Exception ex) 
+            {
+            }
+            
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult LogIn(SigIn logindata) 
+        {
+            bool result = false;
+            SigIn log = meShop.SigIns.Where(x=> x.UserName==logindata.UserName && x.Password==logindata.Password).FirstOrDefault();
+            if (log == null) 
+            {
+                result= true;
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
