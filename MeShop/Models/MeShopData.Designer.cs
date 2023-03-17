7e97bb980392c0e605c658bab0637d598575a309
Lenovo <Lenovo@DESKTOP-QCOO1FJ>
2023-03-17 20:22:41 +0530

    Adding products in Admin Controller

--- MeShop/Models/MeShopData.Designer.cs
+++ MeShop/Models/MeShopData.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Dotnet Practice\Asp.net\14-03-2023\MeShop\Models\MeShopData.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Dotnet Practice\Asp.net\14-03-2023_Meshop\MeShop\Models\MeShopData.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
